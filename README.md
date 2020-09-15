cab2742b0b8a813c955e3705614a3a516451c136
HARScode <hars2pandey@gmail.com>
2020-09-15 23:16:16 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-fdqpwe
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-fdqpwe)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+      <div className="app"> 
+        <h1>Let's Build Covid-19 Tracker </h1>
+        {}
+      </div>
+  );
+}
